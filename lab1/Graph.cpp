bfee733207ae6f691196c18a379af5709aeb82f2
Dmitrii Dronov <dmitrii.dronov@smartsoftdev.eu>
2021-02-10 12:15:49 +0200

    Added data structures for matrices

--- lab1/Graph.cpp
+++ lab1/Graph.cpp
@@ -11,6 +11,30 @@ Graph::Graph(int vertices) :
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        abort();
     }
     
+}
+
+MatrixOfIncidence::MatrixOfIncidence(int numOfPeaks, int numOfEdges) :
+    peaks(numOfPeaks), edges(numOfEdges)
+{
+    short int num{0};
+    for (int i{0}; i < peaks; ++i)
+    {
+        std::vector<short int> vec;
+        for (int j{0}; j < edges; ++j)
+        {
+            do
+            {
+                std::cout << "[" << i << "]" << "[" << j << "] = ";
+                std::cin >> num;
+                if (num != 1 && num != -1 && num != 0)
+                    std::cout << "Invalid number" << std::endl;
+                else
+                    vec.push_back(num);
+            } while (num != 1 && num != -1 && num != 0);
+        }
+        matrix.push_back(vec);
+    }
 }
--- lab1/Graph.hpp
+++ lab1/Graph.hpp
@@ -2,6 +2,7 @@
 #define GRAPH_HPP_
 #include <list>
 #include <memory>
+#include <vector>
 
 class Graph
 {
@@ -11,6 +12,21 @@ private:
 public:
     Graph(int vertices);
     void addEdge(int src, int dest);
+};
+
+class MatrixOfIncidence
+{
+private:
+    std::vector<std::vector<short int>> matrix;
+    //row and columns
+    int peaks, edges;
+public:
+    MatrixOfIncidence(int numOfPeaks, int numOfEdges);
+};
+
+//need to create virual class for both matrices
+class AdjacencyMatrix
+{
 };
 
 #endif
